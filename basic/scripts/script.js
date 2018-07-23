654ccf342b5117e0ef106599755f7d10fb5ac5ca
Zavier <zmyles@comcast.net>
2018-07-23 19:44:46 -0400

    While loop

--- basic/scripts/script.js
+++ basic/scripts/script.js
@@ -1,11 +1,55 @@
+let playerHealth = 40;
+let grant = 10;
+let wins= 0;
+
 const answer = prompt("Do you want to play a game?");
 
 if (answer.toUpperCase() === 'YES') {
-    const playName = prompt("What is your name Warrior?");
+    let playName = prompt("What is your name Warrior?");
 
-    let playerHealth = 40;
-    let grant = 10;
+   
+    while (playerHealth >= 0) {
+            let randomNum = Math.floor(Math.random() * 2 + 1);
+            let randomNum2 = Math.floor(Math.random() * 2 + 1);
+            playerHealth -= randomNum;
+            grant -= randomNum2;
     
-    while ( )
+            console.log(`The player has ${playerHealth} HP!`);
+            console.log(`But grant has ${grant}!`);
+
+            if (playerHealth <= 0) {
+                console.log("Grant wins");
+                continue;
+            }
+
+
+
+            if(grant <= 0){
+                wins++;
+                if (wins >= 3) {
+                    console.log(`${playName} wins `);
+                    break;
+                }
+                grant = 10;
+            }
+
+
+    }
+
+
+
+
+
+
+
+    // while (playerHealth === 40 && grant === 10) {
+    //     let randomNum = Math.floor(Math.random() * 3);
+    //     playerHealth -= randomNum;
+    //     grant -= randomNum;
+
+    //     console.log(`The player has ${playerHealth} HP!`);
+    //     console.log(`But grant has ${grant}!`);
+    // }
+
 } 
 
