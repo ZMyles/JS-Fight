63d5bd1de5cad6c1e2cc8a8ae6825cf4179871f2
Zavier <zmyles@comcast.net>
2018-07-24 16:24:34 -0400

    Functions

--- basic/scripts/script.js
+++ basic/scripts/script.js
@@ -4,52 +4,67 @@ let wins= 0;
 
 const answer = prompt("Do you want to play a game?");
 
+
+//------Damage function -------------------------
+function getDamge() {
+    let randomNum = Math.floor(Math.random() * 5 + 1);
+    return randomNum;
+}
+
+getDamge();
+
+//----- End of Damge function --------------------
+
+
+
+function startCombat() {
+
 if (answer.toUpperCase() === 'YES') {
     let playName = prompt("What is your name Warrior?");
 
-   
+
     while (playerHealth >= 0) {
-            let randomNum = Math.floor(Math.random() * 2 + 1);
-            let randomNum2 = Math.floor(Math.random() * 2 + 1);
-            playerHealth -= randomNum;
-            grant -= randomNum2;
-    
+            // let randomNum = Math.floor(Math.random() * 2 + 1);
+            // let randomNum2 = Math.floor(Math.random() * 2 + 1);
+            // playerHealth -= randomNum;
+            // grant -= randomNum2;
+            
+
+            let fightOn = prompt('Attack or Flee?');
+            
+        // conditional start --------------------
+        if (fightOn.toUpperCase() === "ATTACK") {
+            playerHealth -= getDamge();
+            grant -= getDamge();
             console.log(`The player has ${playerHealth} HP!`);
             console.log(`But grant has ${grant}!`);
 
             if (playerHealth <= 0) {
-                console.log("Grant wins");
-                continue;
+                console.log("Grant wins this round.");
             }
 
 
 
             if(grant <= 0){
+                console.log(`Grant lost this round.`);
                 wins++;
                 if (wins >= 3) {
-                    console.log(`${playName} wins `);
+                    console.log(`${playName} has deafeated the chicken! `);
                     break;
                 }
                 grant = 10;
+                continue
             }
 
-
+        } else {
+            console.log("You escape with your life.");
+            break;
+        } //----------- conditional ends --------------------------
+       
     }
-
-
-
-
-
-
-
-    // while (playerHealth === 40 && grant === 10) {
-    //     let randomNum = Math.floor(Math.random() * 3);
-    //     playerHealth -= randomNum;
-    //     grant -= randomNum;
-
-    //     console.log(`The player has ${playerHealth} HP!`);
-    //     console.log(`But grant has ${grant}!`);
-    // }
-
 } 
 
+
+} //------ End of function --------------
+
+console.log(startCombat());
